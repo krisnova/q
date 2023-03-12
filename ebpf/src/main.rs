c970c355e65b20cb03bb01f0b21cde780c1529d5
Kris Nóva <nova@nivenly.org>
2023-03-12 13:37:28 -0700

    Automatic commit.
    Signed-off-by: Kris Nóva <nova@nivenly.org>

--- ebpf/src/main.rs
+++ ebpf/src/main.rs
@@ -4,8 +4,8 @@
 #[allow(non_snake_case)]
 #[allow(non_camel_case_types)]
 #[allow(dead_code)]
-// mod binding;
-// use crate::binding::{sock, sock_common};
+mod binding;
+use crate::binding::{sock, sock_common};
 use aya_bpf::{helpers::bpf_probe_read_kernel, macros::kprobe, programs::ProbeContext};
 use aya_log_ebpf::info;
 
@@ -13,8 +13,8 @@ use aya_log_ebpf::info;
 #[used]
 pub static LICENSE: [u8; 4] = *b"GPL\0";
 
-// const AF_INET: u16 = 2;
-// const AF_INET6: u16 = 10;
+const AF_INET: u16 = 2;
+const AF_INET6: u16 = 10;
 
 #[kprobe(name = "ebpf")]
 pub fn ebpf(ctx: ProbeContext) -> u32 {
@@ -28,42 +28,36 @@ pub fn ebpf(ctx: ProbeContext) -> u32 {
 }
 
 fn try_ebpf(ctx: ProbeContext) -> Result<u32, i64> {
-    // info!(&ctx, "--");
-    Ok(0)
-    // let sock: *mut sock = ctx.arg(0).ok_or(1i64)?;
-    // let sk_common = unsafe {
-    //     bpf_probe_read_kernel(&(*sock).__sk_common as *const sock_common)
-    //         .map_err(|e| e)?
-    // };
-    // match sk_common.skc_family {
-    //     AF_INET => {
-    //         let src_addr = u32::from_be(unsafe {
-    //             sk_common.__bindgen_anon_1.__bindgen_anon_1.skc_rcv_saddr
-    //         });
-    //         let dest_addr: u32 = u32::from_be(unsafe {
-    //             sk_common.__bindgen_anon_1.__bindgen_anon_1.skc_daddr
-    //         });
-    //         info!(
-    //             &ctx,
-    //             "AF_INET src address: {:ipv4}, dest address: {:ipv4}",
-    //             src_addr,
-    //             dest_addr,
-    //         );
-    //         Ok(0)
-    //     }
-    //     AF_INET6 => {
-    //         let src_addr = sk_common.skc_v6_rcv_saddr;
-    //         let dest_addr = sk_common.skc_v6_daddr;
-    //         info!(
-    //             &ctx,
-    //             "AF_INET6 src addr: {:ipv6}, dest addr: {:ipv6}",
-    //             unsafe { src_addr.in6_u.u6_addr8 },
-    //             unsafe { dest_addr.in6_u.u6_addr8 }
-    //         );
-    //         Ok(0)
-    //     }
-    //     _ => Ok(0),
-    // }
+    let sock: *mut sock = ctx.arg(0).ok_or(1i64)?;
+    let sk_common = unsafe {
+        bpf_probe_read_kernel(&(*sock).__sk_common as *const sock_common).map_err(|e| e)?
+    };
+
+    match sk_common.skc_family {
+        AF_INET => {
+            let src_addr =
+                u32::from_be(unsafe { sk_common.__bindgen_anon_1.__bindgen_anon_1.skc_rcv_saddr });
+            let dest_addr: u32 =
+                u32::from_be(unsafe { sk_common.__bindgen_anon_1.__bindgen_anon_1.skc_daddr });
+            // info!(
+            //     &ctx,
+            //     "AF_INET src address: {:ipv4}, dest address: {:ipv4}", src_addr, dest_addr,
+            // );
+            Ok(0)
+        }
+        AF_INET6 => {
+            let src_addr = sk_common.skc_v6_rcv_saddr;
+            let dest_addr = sk_common.skc_v6_daddr;
+            // info!(
+            //     &ctx,
+            //     "AF_INET6 src addr: {:ipv6}, dest addr: {:ipv6}",
+            //     unsafe { src_addr.in6_u.u6_addr8 },
+            //     unsafe { dest_addr.in6_u.u6_addr8 }
+            // );
+            Ok(0)
+        }
+        _ => Ok(0),
+    }
 }
 
 #[panic_handler]
--- q/src/main.rs
+++ q/src/main.rs
@@ -1,7 +1,7 @@
 use aya::{include_bytes_aligned, programs::KProbe, Bpf};
 use aya_log::BpfLogger;
 use clap::Parser;
-use log::{info, warn};
+use log::{info, warn, LevelFilter};
 use tokio::signal;
 
 #[derive(Debug, Parser)]
@@ -10,22 +10,28 @@ struct Opt {}
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     let _opt = Opt::parse();
-    env_logger::init();
+
+    env_logger::builder()
+        .filter(None, LevelFilter::Trace)
+        .init();
+
+    info!("q: Initializing Program...");
 
     // Compile the eBPF probe directly into the binary
     let mut bpf = Bpf::load(include_bytes_aligned!(
         "../../ebpf/target/bpfel-unknown-none/debug/qprobe"
     ))?;
-    println!("q...");
+    info!("Loaded eBPF probe into kernel");
 
     if let Err(e) = BpfLogger::init(&mut bpf) {
         warn!("failed to initialize eBPF logger: {e}");
     }
+    info!("q: Loading eBPF Programs");
     let program: &mut KProbe = bpf.program_mut("ebpf").unwrap().try_into()?;
     program.load()?;
     program.attach("tcp_connect", 0)?;
 
-    info!("Waiting for Ctrl-C...");
+    info!("q: Waiting for Ctrl-C...");
     signal::ctrl_c().await?;
     info!("Exiting...");
 
